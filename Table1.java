60c0f6f53d5dbf487139982e76d0ab29b290d7ec
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-11-03 18:20:48 +0300

    Add files via upload

---
+++ Table1.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		JOptionPane.showMessageDialog(null, 2-2);
+	}
+
+}
