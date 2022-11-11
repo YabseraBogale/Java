6772a255ef4accd4bdd63219e4b0364bfa7d8375
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-11-11 18:12:56 +0300

    Add files via upload

---
+++ Survey_v2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Survey_v2{
+	public static void main(String[] args){
+		int num1;
+		Scanner input=new Scanner(System.in);
+		int total=0;
+		int times=0;
+		int counter=1;
+		do{
+			for(int i=0;i<3;i++){
+				System.out.printf("%s","Enter number to check Even or not: ");
+				num1=input.nextInt();
+				if(num1%2==0){
+					System.out.printf("%s%d%s%n","Number ",num1," is even.");
+				}
+				else{
+					System.out.printf("%s%d%s%n","Number ",num1," is odd.");
+				}
+				total+=num1;
+				if(i==2){
+					System.out.printf("%s%d%n","Total numbers Calculated is ",total);
+				}
+				times+=1;
+			}
+			System.out.printf("%s","Enter 1 to exit: ");
+			counter=input.nextInt();
+		}while(counter!=1);
+		System.out.printf("%s%d","Number times input taken is ",times);
+	}
+	}
+
