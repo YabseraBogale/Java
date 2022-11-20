3712f264794f9becede93f81f380f4933cd04a2f
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-11-20 13:43:44 +0300

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,25 @@
+public class Account{
+	private String Name;
+	private int Telephone;
+	private String Address;
+	public void SetName(String Name){
+		this.Name=Name;
+	}
+	public void SetTelephone(int Telephone){
+		this.Telephone=Telephone;
+	}
+	public void SetAddress(String Address){
+		this.Address=Address;
+	}
+	public String GetName(){
+		return Name;
+	}
+	public int GetTelephone(){
+		return Telephone;
+	}
+	public String GetAddress(){
+		return Address;
+	}
+	
+		
+	}
