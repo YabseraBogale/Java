a976da68f8f845ec234d063b8c2e3c67b0fa5ea6
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-11-27 17:36:36 +0300

    Add files via upload

---
+++ AccountBalanceTest.java
@@ -0,0 +1,10 @@
+
+public class AccountBalanceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccountBalance account1=new AccountBalance("Yabsera Bogale",3,50.1);
+		System.out.println(account1.GetAge());
+	}
+
+}
