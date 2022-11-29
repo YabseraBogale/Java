454c00adb130207af554cb0b024c47e5349b9c6b
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-11-29 18:24:25 +0300

    Add files via upload

---
+++ TestString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class TestString{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		System.out.printf("Enter input String: ");
+		char newstring=input.next().charAt(0);
+		if(newstring=='D'){
+			System.out.println(newstring);
+		}
+	}
+	}
