b905cfe12d864a761a3c0c8f7ce91ec88589b3a3
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-01-13 18:27:57 +0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,38 @@
+
+public abstract class Employee implements Payable {
+	private final String firstName;
+	private final String lastName;
+	private final String socialSecurityNumber;
+	
+	public Employee(String firstName, String lastName,String socialSecurityNumber)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.socialSecurityNumber = socialSecurityNumber;
+	}
+	// return first name
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	// return last name
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public String getSocialSecurityNumber()
+	{
+		
+		return socialSecurityNumber;
+	}
+	// return String representation of Employee object
+	@Override
+	public String toString()
+	{
+		return String.format("%s %s%nsocial security number: %s",
+					getFirstName(), getLastName(), getSocialSecurityNumber());
+	}
+	// abstract method must be overridden by concrete subclasses
+	
+
+}
